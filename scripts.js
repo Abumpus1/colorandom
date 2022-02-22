d9bafd750ddc08611fb55b3dcd74e036ecfd83fc
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-22 16:49:45 -0700

    Create Color class

---
+++ scripts.js
@@ -0,0 +1,45 @@
+var hexCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9"
+]
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+function createHexCode() {
+  var hexCode = "#";
+  for (var i = 0; i < 6; i++) {
+    hexCode += hexCharacters[getRandomIndex(hexCharacters)]
+  }
+  return hexCode;
+};
+
+console.log(createHexCode());
+
+class Color {
+  constructor() {
+    hexCode = createHexCode()
+    locked = false;
+  }
+};
+
+class Palette {
+  constructor() {
+
+  }
+};
